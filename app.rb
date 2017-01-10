569833f53c1b42f62f4eb510d5536cd07d53aced
Ujjwal Bhardwaj <ujjwalb1996@gmail.com>
2017-01-11 00:30:33 +0530

    Updated with user registration

---
+++ app.rb
@@ -0,0 +1,77 @@
+require 'rubygems'
+require 'sinatra'
+require 'data_mapper'
+require 'sinatra/namespace'
+require 'json'
+
+# adapted from http://datamapper.org/getting-started.html
+# DataMapper.setup(:default, 'mysql://user:password@hostname/database')
+DataMapper.setup(:default, 'mysql://root:@localhost/database_name')
+
+# User Model
+class User
+  include DataMapper::Resource
+
+  property :id,         	Serial
+  property :username,   	String
+  property :email,			String
+  property :password,   	BCryptHash
+  property :first_name, 	String
+  property :last_name,  	String
+  property :date_of_birth, 	String
+  property :created_at,    	DateTime
+end
+
+DataMapper.finalize
+DataMapper.auto_migrate!
+
+helpers do
+  def valid_email? (email)
+    regex = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+    if email =~ regex
+      return true
+    end
+    return false
+  end
+end
+
+namespace '/api/v1/user' do
+  before do
+    content_type 'application/json'
+  end
+
+  post '/register/' do
+    first_name      = params[:first_name].nil?      ? '' : params[:first_name] 
+    last_name       = params[:last_name].nil?       ? '' : params[:last_name]
+    username        = params[:username].nil?        ? '' : params[:username]
+    email           = params[:email].nil?           ? '' : params[:email]
+    password        = params[:password].nil?        ? '' : params[:password]
+    password_repeat = params[:password_repeat].nil? ? '' : params[:password_repeat]
+        
+    if first_name.empty? or last_name.empty? or username.empty? or email.empty? or password.empty? or password_repeat.empty?
+      halt 400, { message: "Empty Credentials." }.to_json
+    end
+    
+    if !valid_email?(email)
+      halt 400, { message: "Invalid Email id" }.to_json
+    end
+
+    if password != password_repeat
+      halt 400, { message: "Passwords do not match." }.to_json
+    end
+    if Users.count(:conditions => ['username = ?', username]) != 0
+      halt 400, { message: "User with given username already exists." }.to_json
+    end
+
+    if Users.count(:conditions => ['email = ?', email]) != 0
+      halt 400, { message: "User with given email id already exists" }.to_json
+    end 
+
+    user = Users.create(:username => username, :email => email, :password => password, :first_name => first_name, :last_name => last_name)
+    if !user.saved?
+      halt 400, { message: "Failed to register user." }.to_json
+    end
+    halt 200, { message: "User registered successfully." }.to_json
+  end
+
+end
